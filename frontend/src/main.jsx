14e6dc9e8351321fcf759ac057477ab888557fe2
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2025-10-17 23:02:49 +0530

    fix basename issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/royalty-stream'>
+    <BrowserRouter basename={import.meta.env.VITE_BASE_URL || '/'}>
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}>
         <App />
       </GoogleOAuthProvider>
